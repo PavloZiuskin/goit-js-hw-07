e2753473748e6cad831591761d5bcf7eea29a649
PavloZiuskin <passzuskin@gmail.com>
2022-12-17 13:28:11 +0200

    add-task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+const newGallaryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li>
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+    </li>`;
+  })
+  .join("");
+
+refs.gallery.insertAdjacentHTML("beforeend", newGallaryMarkup);
+new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
